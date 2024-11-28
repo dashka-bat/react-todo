a2d23f63cc2d51c5266fa8626db2787deb649049
dashka-bat <dushkab02@gmail.com>
2024-11-28 18:19:05 +0800

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,51 @@
-
-import "./style.css"
-
-let task=[]
-function render(){
-  let output=document.getElementById("onetask")
-  output.innerHTML=""
-  for(let i=0;i<task.length;i++){
-    output.innerHTML+=`<div id="onetask" className="onetask"><input type="checkbox"></input>`+task[i].name
-
-
+import "./style.css";
+function addtask() {
+  const name = input - text.value;
+  task.push({ name: name, status: "todo" });
+}
+let task = [];
+function Render() {
+  let output = document.getElementById("onetask");
+  output.innerHTML = "";
+  for (let i = 0; i < task.length; i++) {
+    output.innerHTML +=
+      `<div id="onetask" className="onetask"><input type="checkbox"></input>` +
+      task[i].name +
+      `</div>`;
   }
+  Render();
+  return (
+    <div className="background">
+      <h1 className="task-list">TASK LIST</h1>
+      <div>
+        <button onClick={addtask()} className="add-button">
+          +
+        </button>
+        <button className="deleteall-button">delete all</button>
+        <input
+          id="input-text"
+          className="input-text"
+          type="text"
+          placeholder="do something lazy ass"
+        ></input>
+        <div id="onetask" className="onetask">
+          <input type="checkbox"></input>~
+        </div>
+      </div>
+      <render />
+      <addtask />
+    </div>
+  );
 }
 
 // function addtask(){
 //   const name = input-text.value
 //   task.push({name:name})}
 
-
 export default function Home() {
   return (
-   <div className="background">
-    <h1 className="task-list">TASK LIST</h1>
-    <div>
-    <button className="add-button">+</button>
-    <button className="deleteall-button">delete all</button>
-      <input id="input" className="input-text" type="text"placeholder="do something lazy ass"></input>
-      <div id="onetask" className="onetask">
-        <input type="checkbox"></input>
-~
-      </div>
-
-         </div>
-    <render/>
-    <addtask/>
-   </div>
+    <>
+      <Render />
+    </>
   );
 }
