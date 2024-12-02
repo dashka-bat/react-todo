e2493118c85a572434c5d77720e390a77098da01
dashka-bat <dushkab02@gmail.com>
2024-12-02 17:28:12 +0800

    lesson

--- src/app/page.js
+++ src/app/page.js
@@ -1,82 +1,65 @@
-
 "use client";
 import { useState } from "react";
 
-import "./style.css"
-
+import "./style.css";
 
 export default function Home() {
-  const [Open,setopen]= useState(false);
-  const addmodal=() =>{
+  const [Open, setopen] = useState(false);
+  const addmodal = () => {
     setopen(true);
   };
-  const closemodal=() =>{
-    setopen(false)
-    
+  const closemodal = () => {
+    setopen(false);
   };
-  
+
   return (
+    <div>
+      <div className="body">
+        <div>
+          <button className="add-button" onClick={addmodal}>
+            +
+          </button>
+          {Open && (
+            <div>
+              <div
+                style={{
+                  marginTop: 20,
+                  height: 500,
+                  width: 800,
+                  backgroundColor: "pink",
+                  margin: "auto",
+                  padding: 0,
+                  color: "black",
+                  fontSize: "2rem",
+                }}
+              >
+                <button className="close-button" onClick={closemodal}>
+                  X
+                </button>
+                <input
+                  style={{
+                    border: "2px solid black",
+                    width: 256,
+                    height: 56,
+                    marginLeft: "12vw",
 
-   <div>
-    <div className="body">
-      <div>
-<button className="add-button" onClick={addmodal}>+</button>
-{Open && (
-<div >
-<div style={{
-  marginTop:20,
-  height:500,
-  width:800,
-  backgroundColor:"pink",
-  margin:"auto",
-  padding:0,color:"black"
-}}>
-<button className="close-button" onClick={closemodal}>X
-</button>
-<input style={{border:"2px solid black",
-                width: 256,
-                height: 56,
-                marginLeft:"12vw",
-               
-                border: "1px solid black",
-                
-                color:"black"}} type="task">
-                  
+                    border: "1px solid black",
 
+                    color: "black",
+                  }}
+                  type="task"
+                ></input>
 
+                <button className="submit" style={{ display: "flex" }}>
+                  submit
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
 
-</input>
-
-
-<button style={{display:"flex"}}>submit</button>
-
-</div>
-
-
-
-
-
-</div>
-
-
-
-
-
-)
-
-
-
-}
+        <footer>{Open}</footer>
       </div>
-
-
-
-<footer>{Open}
-</footer>
-
     </div>
-
-   </div>
-   
   );
 }
